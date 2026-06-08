b030d3157a78b20ead75241818752d306930d4ff
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-06-09 01:33:01 +0600

    Add Cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+      protected $fillable = [
+        'title',
+        'description',
+        'quantity',
+        'price',
+        'image',
+        'category_id',
+       
+    ];
+
+    public function category(){
+
+        return $this->belongsTo(Category::class);
+    }
+}
