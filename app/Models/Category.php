f5b9034ebfe0d1116fad0cd2a0c15cdeaa4090e5
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-06-09 01:35:41 +0600

    Add Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+   
+
+     
+     protected $fillable = [
+        'cat_title',
+       
+    ];
+
+     public function carts()
+    {
+        return $this->hasMany(Cart::class, 'category_id');
+    }
+}
