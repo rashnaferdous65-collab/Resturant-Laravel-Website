c94c6fdc5febd6ff685bede6239a84c1580d2333
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-06-10 00:42:51 +0600

    Add Food model

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Food extends Model
+{
+      protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'image',
+        'category_id',
+       
+    ];
+
+    public function category(){
+
+        return $this->belongsTo(Category::class);
+    }
+}
