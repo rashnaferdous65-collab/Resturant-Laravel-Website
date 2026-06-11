eda400a141d03c9e252433e6308b82e5322ece39
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-06-11 21:49:08 +0600

    Add Table_book model

--- app/Models/Table_book.php
+++ app/Models/Table_book.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Table_book extends Model
+{
+         protected $fillable = [
+        'phone',
+        'guest',
+        'date',
+        'time',
+       
+       
+    ];
+}
