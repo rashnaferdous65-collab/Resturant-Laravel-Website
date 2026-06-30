5f68d2ce6db84aac9a8ff045f8bddabe98bca3be
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-01 03:42:39 +0600

    Update primary-button component design

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,35 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+```php
+@props(['type' => 'submit'])
+
+<button
+    type="{{ $type }}"
+    {{ $attributes->class([
+        'inline-flex',
+        'items-center',
+        'px-5',
+        'py-2',
+        'rounded-lg',
+        'border',
+        'border-transparent',
+        'bg-gray-800',
+        'text-white',
+        'text-xs',
+        'font-semibold',
+        'uppercase',
+        'tracking-wide',
+        'transition-all',
+        'duration-150',
+        'ease-in-out',
+        'hover:bg-gray-700',
+        'focus:bg-gray-700',
+        'active:bg-gray-900',
+        'focus:outline-none',
+        'focus:ring-2',
+        'focus:ring-indigo-500',
+        'focus:ring-offset-2'
+    ]) }}
+>
     {{ $slot }}
 </button>
+```
+
