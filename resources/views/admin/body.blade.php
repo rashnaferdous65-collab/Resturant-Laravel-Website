3b3371acaef310b3051282b22e7f96d0d5a133e1
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-08 20:10:53 +0600

    Create admin category blade view

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -1,4 +1,4 @@
-          <div class="page-content">
+ <div class="page-content">
         <div class="page-header">
           <div class="container-fluid">
             <h2 class="h5 no-margin-bottom">Dashboard</h2>
