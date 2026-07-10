bffca8c6143d3ffc82ce4f1d6b9db3a4f40729b1
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-10 23:23:18 +0600

    Update admin edit category page layout

--- resources/views/admin/edit_category.blade.php
+++ resources/views/admin/edit_category.blade.php
@@ -2,72 +2,95 @@
 <html lang="en">
 <head>
 
- <style>
+@include('admin.css')
 
+<style>
 
-.category{
+.wrapper{
+    display: flex;
+    justify-content: center;
+    margin-top: 40px;
+}
 
-
+.edit-box{
     text-align: center;
-    margin: auto;
 }
 
-.cat{
-
+.title{
+    color: #fff;
     font-weight: bold;
-    color: white;
-    padding-bottom: 50px;
+    margin-bottom: 35px;
 }
 
-.field{
-
-    width: 500px;
-    height: 40px;
+.input-box{
+    width: 480px;
+    height: 42px;
+    padding: 8px 12px;
+    margin: 15px 0 25px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
 }
-   
 
- </style>
-    @include('admin.css')
+</style>
+
 </head>
+
 <body>
 
-    @include('admin.header')
-    @include('admin.slidebar')
+@include('admin.header')
+@include('admin.slidebar')
 
-    <div class="page-content">
-        <div class="page-header">
-            <div class="container-fluid">
-               
-             @if(session()->has('message'))
-                  <div class="alert alert-success alert-dismissible fade show text-center mx-auto" role="alert" style="max-width: 600px; margin-top: 20px;">
-              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">x</button>
+<div class="page-content">
+    <div class="page-header">
+        <div class="container-fluid">
 
-             
-               {{session()->get('message')}}
+            @if(session('message'))
+                <div class="alert alert-success alert-dismissible fade show mx-auto text-center" style="max-width:600px;">
+                    <button type="button" class="btn-close" data-bs-dismiss="alert">x</button>
+
+                    {{ session('message') }}
                 </div>
-               @endif
-               <div class="category">
-               <h1 class="cat">Edit Category</h1>
-                  <form action="{{ route('update_category', $data->id) }}" method="POST">
-    @csrf
-    @method('PUT')
+            @endif
 
-    <label>Category Name</label>
-    <input 
-        type="text" 
-        name="cat_name" 
-        value="{{ old('cat_name', $data->cat_title) }}" 
-        class="field" 
-        required
-    >
+            <div class="wrapper">
 
-    <button class="btn btn-primary"  type="submit">Update Category</button>
-</form>
+                <div class="edit-box">
 
-            
-              
-               </div> 
+                    <h2 class="title">Update Category</h2>
+
+                    <form action="{{ route('update_category', $data->id) }}" method="POST">
+
+                        @csrf
+                        @method('PUT')
+
+                        <div>
+                            <label class="text-white d-block mb-2">
+                                Category Name
+                            </label>
+
+                            <input
+                                type="text"
+                                name="cat_name"
+                                class="input-box"
+                                value="{{ old('cat_name', $data->cat_title) }}"
+                                required
+                            >
+                        </div>
+
+                        <button type="submit" class="btn btn-success px-4">
+                            Save Changes
+                        </button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 </div>
-</div>
-</div>
-</div>
+
+</body>
+</html>
