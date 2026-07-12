50584ddb8ba45db4ab08386cc84314553296dbd6
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-12 22:32:35 +0600

    Update admin footer Blade template

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -1,22 +1,28 @@
-       <!-- Footer-->
-        <footer class="footer">
-          <div class="footer__block block no-margin-bottom">
-            <div class="container-fluid text-center">
-             
-               <p class="no-margin-bottom"> Project By 2025 &copy;Rashna Ferdous</p>
+<!-- Footer Section -->
+<footer class="footer">
+    <div class="footer__block block mb-0">
+        <div class="container-fluid">
+            <div class="text-center">
+                <p class="mb-0">
+                    &copy; 2025 Project by <strong>Rashna Ferdous</strong>
+                </p>
             </div>
-          </div>
-        </footer>
-      </div>
+        </div>
     </div>
-    <!-- JavaScript files--> 
-    <script src="{{asset('admin_css/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('admin_css/vendor/popper.js/umd/popper.min.js')}}"> </script>
-    <script src=" {{asset('admin_css/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('admin_css/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
-    <script src="{{asset('admin_css/vendor/chart.js/Chart.min.js')}}"></script>
-    <script src=" {{asset('admin_css/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
-    <script src="{{asset('admin_css/js/charts-home.js')}}"></script>
-    <script src="{{asset('admin_css/js/front.js')}}"></script>
-  </body>
+</footer>
+
+</div>
+</div>
+
+<!-- JavaScript -->
+<script src="{{ asset('admin_css/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('admin_css/vendor/popper.js/umd/popper.min.js') }}"></script>
+<script src="{{ asset('admin_css/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('admin_css/vendor/jquery.cookie/jquery.cookie.js') }}"></script>
+<script src="{{ asset('admin_css/vendor/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('admin_css/vendor/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('admin_css/js/charts-home.js') }}"></script>
+<script src="{{ asset('admin_css/js/front.js') }}"></script>
+
+</body>
 </html>
