6774ce1a5f6b167655850fa10d53c35af0f070df
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-16 22:46:36 +0600

    Update admin slidebar blade

--- resources/views/admin/slidebar.blade.php
+++ resources/views/admin/slidebar.blade.php
@@ -1,29 +1,77 @@
- <div class="d-flex align-items-stretch">
-      <!-- Sidebar Navigation-->
-      <nav id="sidebar">
-        <!-- Sidebar Header-->
-        <div class="sidebar-header d-flex align-items-center">
-          <div class="avatar"><img src="{{asset('admin_css/img/avatar-6.jpg')}}" alt="..." class="img-fluid rounded-circle"></div>
-          <div class="title">
-            <h1 class="h5">Mark Stephen</h1>
-            <p>Web Designer</p>
-          </div>
+<div class="d-flex align-items-stretch">
+
+    <!-- Sidebar Start -->
+    <nav id="sidebar">
+
+        <!-- Profile -->
+        <div class="sidebar-header">
+            <div class="d-flex align-items-center">
+                <div class="avatar">
+                    <img src="{{ asset('admin_css/img/avatar-6.jpg') }}"
+                         alt="Profile"
+                         class="img-fluid rounded-circle">
+                </div>
+
+                <div class="title ml-3">
+                    <h1 class="h5 mb-1">Mark Stephen</h1>
+                    <p class="mb-0">Web Designer</p>
+                </div>
+            </div>
         </div>
-        <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+
+        <span class="heading">Main</span>
+
         <ul class="list-unstyled">
-                <li class="active"><a href="{{url('admin/dashboard')}}"> <i class="icon-home"></i>Home </a></li>
-                <li class=""><a href="{{url('category')}}"> <i class="icon-grid"></i>Food Category </a></li>
-                <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Food</a>
-                  <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                    <li><a href="{{url('add_food')}}">Add Food Details</a></li>
-                    <li><a href="{{url('view_food')}}">View Food Detalis</a></li>
-                   
-                  </ul>
-                </li>
-                <li class=""><a href="{{url('view_order')}}"> <i class="icon-user"></i>View Order </a></li>
-                  <li class=""><a href="{{url('reservation')}}"> <i class="icon-user"></i>View Reservation </a></li>
-           
-           
+
+            <li class="active">
+                <a href="{{ url('admin/dashboard') }}">
+                    <i class="icon-home"></i>
+                    <span>Home</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="{{ url('category') }}">
+                    <i class="icon-grid"></i>
+                    <span>Food Category</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#foodMenu"
+                   data-toggle="collapse"
+                   aria-expanded="false">
+                    <i class="icon-windows"></i>
+                    <span>Food</span>
+                </a>
+
+                <ul id="foodMenu" class="collapse list-unstyled">
+                    <li>
+                        <a href="{{ url('add_food') }}">Add Food Details</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('view_food') }}">View Food Details</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="{{ url('view_order') }}">
+                    <i class="icon-user"></i>
+                    <span>View Order</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="{{ url('reservation') }}">
+                    <i class="icon-user"></i>
+                    <span>View Reservation</span>
+                </a>
+            </li>
+
         </ul>
-      </nav>
-      <!-- Sidebar Navigation end-->
+
+    </nav>
+    <!-- Sidebar End -->
+
+</div>
