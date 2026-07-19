3c242e76b98dbb960c27f41dfa86cfe1238c1564
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-19 22:29:44 +0600

    Update home review blade view

--- resources/views/home/review.blade.php
+++ resources/views/home/review.blade.php
@@ -1,32 +1,64 @@
-   <div id="testmonial" class="container-fluid wow fadeIn bg-dark text-light has-height-lg middle-items">
-        <h2 class="section-title my-5 text-center">REVIEWS</h2>
-        <div class="row mt-3 mb-5">
-            <div class="col-md-4 my-3 my-md-0">
-                <div class="testmonial-card">
-                    <h3 class="testmonial-title">John Doe</h3>
-                    <h6 class="testmonial-subtitle">Web Designer</h6>
-                    <div class="testmonial-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum nobis eligendi, quaerat accusamus ipsum sequi dignissimos consequuntur blanditiis natus. Aperiam!</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 my-3 my-md-0">
-                <div class="testmonial-card">
-                    <h3 class="testmonial-title">Steve Thomas</h3>
-                    <h6 class="testmonial-subtitle">UX/UI Designer</h6>
-                    <div class="testmonial-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum minus obcaecati cum eligendi perferendis magni dolorum ipsum magnam, sunt reiciendis natus. Aperiam!</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 my-3 my-md-0">
-                <div class="testmonial-card">
-                    <h3 class="testmonial-title">Miranda Joy</h3>
-                    <h6 class="testmonial-subtitle">Graphic Designer</h6>
-                    <div class="testmonial-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, nam. Earum nobis eligendi, dignissimos consequuntur blanditiis natus. Aperiam!</p>
-                    </div>
-                </div>
-            </div>
+<section id="testmonial" class="container-fluid bg-dark text-light has-height-lg middle-items wow fadeIn">
+    <div class="container py-5">
+
+        <div class="text-center mb-5">
+            <h2 class="section-title">REVIEWS</h2>
         </div>
+
+        <div class="row g-4">
+
+            <div class="col-lg-4 col-md-6">
+                <div class="testmonial-card h-100 p-4">
+                    <div class="mb-3">
+                        <h4 class="testmonial-title mb-1">John Doe</h4>
+                        <small class="testmonial-subtitle">Web Designer</small>
+                    </div>
+
+                    <div class="testmonial-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Earum nobis eligendi, quaerat accusamus ipsum sequi
+                            dignissimos consequuntur blanditiis natus. Aperiam!
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="testmonial-card h-100 p-4">
+                    <div class="mb-3">
+                        <h4 class="testmonial-title mb-1">Steve Thomas</h4>
+                        <small class="testmonial-subtitle">UX/UI Designer</small>
+                    </div>
+
+                    <div class="testmonial-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Laborum minus obcaecati cum eligendi perferendis magni
+                            dolorum ipsum magnam, sunt reiciendis natus. Aperiam!
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="testmonial-card h-100 p-4">
+                    <div class="mb-3">
+                        <h4 class="testmonial-title mb-1">Miranda Joy</h4>
+                        <small class="testmonial-subtitle">Graphic Designer</small>
+                    </div>
+
+                    <div class="testmonial-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Aliquid, nam. Earum nobis eligendi, dignissimos
+                            consequuntur blanditiis natus. Aperiam!
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
+</section>
