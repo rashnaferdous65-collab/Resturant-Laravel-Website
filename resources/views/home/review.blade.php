e268f516ac3c83c0c8058e117c8fd313e1ef68e4
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-20 23:17:04 +0600

    Update admin navbar layout

--- resources/views/home/review.blade.php
+++ resources/views/home/review.blade.php
@@ -1,60 +1,67 @@
 <section id="testmonial" class="container-fluid bg-dark text-light has-height-lg middle-items wow fadeIn">
     <div class="container py-5">
 
-        <div class="text-center mb-5">
-            <h2 class="section-title">REVIEWS</h2>
-        </div>
+        <h2 class="section-title text-center mb-5">REVIEWS</h2>
 
-        <div class="row g-4">
+        <div class="row">
 
-            <div class="col-lg-4 col-md-6">
-                <div class="testmonial-card h-100 p-4">
-                    <div class="mb-3">
-                        <h4 class="testmonial-title mb-1">John Doe</h4>
-                        <small class="testmonial-subtitle">Web Designer</small>
-                    </div>
+            <!-- Review 1 -->
+            <div class="col-md-6 col-lg-4 mb-4">
+                <div class="testmonial-card p-4 h-100">
+
+                    <h4 class="testmonial-title">John Doe</h4>
+                    <span class="testmonial-subtitle d-block mb-3">
+                        Web Designer
+                    </span>
 
                     <div class="testmonial-body">
-                        <p>
+                        <p class="mb-0">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                             Earum nobis eligendi, quaerat accusamus ipsum sequi
                             dignissimos consequuntur blanditiis natus. Aperiam!
                         </p>
                     </div>
+
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6">
-                <div class="testmonial-card h-100 p-4">
-                    <div class="mb-3">
-                        <h4 class="testmonial-title mb-1">Steve Thomas</h4>
-                        <small class="testmonial-subtitle">UX/UI Designer</small>
-                    </div>
+            <!-- Review 2 -->
+            <div class="col-md-6 col-lg-4 mb-4">
+                <div class="testmonial-card p-4 h-100">
+
+                    <h4 class="testmonial-title">Steve Thomas</h4>
+                    <span class="testmonial-subtitle d-block mb-3">
+                        UX/UI Designer
+                    </span>
 
                     <div class="testmonial-body">
-                        <p>
+                        <p class="mb-0">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                             Laborum minus obcaecati cum eligendi perferendis magni
                             dolorum ipsum magnam, sunt reiciendis natus. Aperiam!
                         </p>
                     </div>
+
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6">
-                <div class="testmonial-card h-100 p-4">
-                    <div class="mb-3">
-                        <h4 class="testmonial-title mb-1">Miranda Joy</h4>
-                        <small class="testmonial-subtitle">Graphic Designer</small>
-                    </div>
+            <!-- Review 3 -->
+            <div class="col-md-6 col-lg-4 mb-4">
+                <div class="testmonial-card p-4 h-100">
+
+                    <h4 class="testmonial-title">Miranda Joy</h4>
+                    <span class="testmonial-subtitle d-block mb-3">
+                        Graphic Designer
+                    </span>
 
                     <div class="testmonial-body">
-                        <p>
+                        <p class="mb-0">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                             Aliquid, nam. Earum nobis eligendi, dignissimos
                             consequuntur blanditiis natus. Aperiam!
                         </p>
                     </div>
+
                 </div>
             </div>
 
