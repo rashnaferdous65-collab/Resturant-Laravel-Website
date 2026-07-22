2e0c7e705098f1f41c4fd1399c38de98a1ee1fbe
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-22 23:32:58 +0600

    Refactor home index blade structure

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,32 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-@include('home.css')
 
-    
-    <!-- Navbar -->
-   @include('home.navbar')
-    <!-- header -->
-  
-  @include('home.header')
-    <!--  About Section  -->
-   
-  @include('home.about')
-    <!--  gallary Section  -->
+<head>
+    @include('home.css')
+</head>
+
+<body>
+
+    {{-- Navbar --}}
+    @include('home.navbar')
+
+    {{-- Header --}}
+    @include('home.header')
+
+    {{-- About --}}
+    @include('home.about')
+
+    {{-- Gallery --}}
     @include('home.gallery')
 
-    <!-- book a table Section  -->
-   @include('home.book_table')
+    {{-- Book Table --}}
+    @include('home.book_table')
 
-    <!-- BLOG Section  -->
-   
-@include('home.blog')
-    <!-- REVIEWS Section  -->
- 
-@include('home.review')
-    <!-- CONTACT Section  -->
-  
-@include('home.contact')
-    <!-- page footer  -->
+    {{-- Blog --}}
+    @include('home.blog')
 
+    {{-- Reviews --}}
+    @include('home.review')
+
+    {{-- Contact --}}
+    @include('home.contact')
+
+    {{-- Footer --}}
     @include('home.footer')
+
+</body>
+
+</html>
    
