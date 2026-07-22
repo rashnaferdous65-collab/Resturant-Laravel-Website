dc71d9bdec38349801293d5f991d0b1ec84b2318
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-22 23:29:21 +0600

    Add home index blade template

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('home.css')
+
+    
+    <!-- Navbar -->
+   @include('home.navbar')
+    <!-- header -->
+  
+  @include('home.header')
+    <!--  About Section  -->
+   
+  @include('home.about')
+    <!--  gallary Section  -->
+    @include('home.gallery')
+
+    <!-- book a table Section  -->
+   @include('home.book_table')
+
+    <!-- BLOG Section  -->
+   
+@include('home.blog')
+    <!-- REVIEWS Section  -->
+ 
+@include('home.review')
+    <!-- CONTACT Section  -->
+  
+@include('home.contact')
+    <!-- page footer  -->
+
+    @include('home.footer')
+   
