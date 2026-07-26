daf78b095086ccd0182fefab685182579cfc3ab4
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-26 23:09:42 +0600

    Update css.blade.php in home view

---
+++ resources/views/home/css.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with FoodHut landing page.">
+    <meta name="author" content="Devcrud">
+    <title>FoodHut</title>
+   
+    <link rel="stylesheet" href="{{ asset('assets/vendors/themify-icons/css/themify-icons.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets/vendors/animate/animate.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/foodhut.css') }}">
+</head>
