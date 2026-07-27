dd38379039d1568d8ee45ee14b24de7d334f5226
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-07-27 22:26:48 +0600

    Update contact.blade.php layout

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,16 +1,42 @@
-  <div id="contact" class="container-fluid bg-dark text-light border-top wow fadeIn">
-        <div class="row">
-            <div class="col-md-6 px-0">
-                <div id="map" style="width: 100%; height: 100%; min-height: 400px"></div>
-            </div>
-            <div class="col-md-6 px-5 has-height-lg middle-items">
-                <h3>FIND US</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, laboriosam doloremque odio delectus, sunt magnam laborum impedit molestiae, magni quae ipsum, ullam eos! Alias suscipit impedit et, adipisci illo quam.</p>
-                <div class="text-muted">
-                    <p><span class="ti-location-pin pr-3"></span> 12345 Fake ST NoWhere, AB Country</p>
-                    <p><span class="ti-support pr-3"></span> (123) 456-7890</p>
-                    <p><span class="ti-email pr-3"></span>info@website.com</p>
-                </div>
-            </div>
+<div id="contact" class="container-fluid bg-dark text-light border-top wow fadeIn">
+    <div class="row">
+
+        <!-- Google Map -->
+        <div class="col-md-6 p-0">
+            <div id="map" style="min-height:400px; width:100%; height:100%;"></div>
         </div>
+
+        <!-- Contact Information -->
+        <div class="col-md-6 px-5 has-height-lg middle-items">
+
+            <h3 class="mb-4">FIND US</h3>
+
+            <p class="mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus, repudiandae. Eaque asperiores laboriosam
+                cupiditate, suscipit quia unde sint ipsum pariatur.
+            </p>
+
+            <div class="text-muted">
+
+                <div class="mb-3">
+                    <span class="ti-location-pin pr-3"></span>
+                    <span>12345 Fake ST NoWhere, AB Country</span>
+                </div>
+
+                <div class="mb-3">
+                    <span class="ti-support pr-3"></span>
+                    <span>(123) 456-7890</span>
+                </div>
+
+                <div>
+                    <span class="ti-email pr-3"></span>
+                    <span>info@website.com</span>
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
+</div>
